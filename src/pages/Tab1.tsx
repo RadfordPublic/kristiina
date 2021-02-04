a9f02e1a191c320e7545b65a67203adcf1c61a10
Sam Radford <sam.radford@capitalroad.com.au>
2021-02-05 00:15:49 +1100

    fixed up a few things added some graphs made marketplace look nicer added links between them all

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,63 +1,65 @@
 import React from "react";
 import {
-    IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol,
-    IonContent, IonGrid, IonItem, IonLabel, IonRow, IonThumbnail,
+    IonCard, IonCardContent, IonCol,
+    IonContent, IonGrid, IonLabel, IonRow,
 } from "@ionic/react";
 
 const Tab1: React.FC = () => {
 
-  return (
-      <IonContent>
-          <IonGrid>
-              <IonRow>
-                  <IonCol size={"12"}>
-                      <IonCard>
-                          <IonCardHeader>
-                              <IonCardTitle>Martha Kalifatidis @marthaa__k</IonCardTitle>
-                          </IonCardHeader>
-                          <IonCardContent>
-                              <IonItem>
-                                  <IonThumbnail slot="start">
-                                      <img alt={"model"}
-                                           src="https://s.yimg.com/ny/api/res/1.2/79yF4GQkOFYdSQZ4eBgqEw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTExOTguOA--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2020-08/45a57320-e014-11ea-9a96-395aac96b517"/>
-                                  </IonThumbnail>
-                                  <IonLabel>
-                                      <p> 870 posts 430k followers 1,581 following</p>
-                                  </IonLabel>
-                              </IonItem>
-                              {/*<IonButton*/}
-                              {/*    expand="full"*/}
-                              {/*    onClick={() => history.push("/page/Profile/tabs/tab1-detail")}*/}
-                              {/*>*/}
-                              {/*    NEXT PAGE*/}
-                              {/*</IonButton>*/}
-                          </IonCardContent>
-                      </IonCard>
-                  </IonCol>
-              </IonRow>
-              <IonRow>
-                  <IonCol size={"12"}>
-                      <IonCard>
-                          <IonCardContent>
-                              <IonLabel>
-                                  <p>This will be content feed, need to sort out auth tokens with facebook to get this to come through</p>
-                              </IonLabel>
-                              <img alt={"model"}
-                                   src="https://i.imgur.com/wvW7JZ7.png"/>
-                              {/*<Iframe url="https://imgur.com/wvW7JZ7"*/}
-                              {/*        width="450px"*/}
-                              {/*        height="450px"*/}
-                              {/*        id="myId"*/}
-                              {/*        className="myClassname"*/}
-                              {/*        display="inline"*/}
-                              {/*        position="relative"/>*/}
-                          </IonCardContent>
-                      </IonCard>
-                  </IonCol>
-              </IonRow>
-          </IonGrid>
+    return (
+        <IonContent>
+            <IonGrid>
+                <IonRow>
+                    <IonCol size={"12"}>
+                        <IonCard>
+                            <IonCardContent>
+                                <IonGrid>
+                                    <IonRow>
+                                        <IonCol size={"3"}>
+                                            <img alt={'profile'} style={{
+                                                display: "block",
+                                                marginLeft: "auto",
+                                                marginRight: "auto",
+                                                height: "150px",
+                                                width: "150px",
+                                                borderRadius: "50%",
+                                            }}
+                                                 src="https://s.yimg.com/ny/api/res/1.2/79yF4GQkOFYdSQZ4eBgqEw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTExOTguOA--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2020-08/45a57320-e014-11ea-9a96-395aac96b517"/>
+                                        </IonCol>
+                                        <IonCol size={"9"}>
+                                            <IonLabel>
+                                                <h1>Martha Kalifatidis</h1>
+                                                <h2>@marthaa__k</h2>
+                                                <br/>
+                                                <p> 870 posts 430k followers 1,581 following</p>
+                                            </IonLabel>
+                                        </IonCol>
+                                    </IonRow>
+                                </IonGrid>
+                            </IonCardContent>
+                        </IonCard>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol size={"12"}>
+                        <IonCard>
+                            <IonCardContent>
+                                <img alt={"model"}
+                                     src="https://i.imgur.com/wvW7JZ7.png"/>
+                                {/*<Iframe url="https://imgur.com/wvW7JZ7"*/}
+                                {/*        width="450px"*/}
+                                {/*        height="450px"*/}
+                                {/*        id="myId"*/}
+                                {/*        className="myClassname"*/}
+                                {/*        display="inline"*/}
+                                {/*        position="relative"/>*/}
+                            </IonCardContent>
+                        </IonCard>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
         </IonContent>
-  );
+    );
 };
 
 export default Tab1;
